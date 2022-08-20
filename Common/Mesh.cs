a328553e990be8d7f6bb065593c342b8cf425cf8
lceCoIdMoon <wehuffman15@gmail.com>
2022-08-20 17:27:19 -0400

    updates

---
+++ Common/Mesh.cs
@@ -0,0 +1,13 @@
+using OpenTK.Mathematics;
+
+namespace Nebula;
+
+public class Mesh {
+
+}
+
+class Vertex {
+    public Vector3 Position;
+    public Vector3 Normal;
+    public Vector2 TexCoords;
+}
